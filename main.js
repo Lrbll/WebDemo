5d8b41591ee0e6653480aa255370d5b239f3ee21
Lrbll <dlxkdxs@naver.com>
2023-03-16 05:12:16 +0900

    2nd

--- main.js
+++ main.js
@@ -18,14 +18,20 @@ function reset() {
 
 function showHTML() {
   document.getElementById("fig").src = "img/html.png"
+  document.getElementById("desc").innerHTML =
+    "<b>HTML</b>을 이용하여, 웹의 구조를 지정했어요"
 }
 
 function showCSS() {
   document.getElementById("fig").src = "img/css.png"
+  document.getElementById("desc").innerHTML =
+    "<b>CSS</b>를 이용하여, 스타일을 입혔어요"
 }
 
 function showJS() {
   document.getElementById("fig").src = "img/JS.png"
+  document.getElementById("desc").innerHTML =
+    "<b>Javascript</b>를 이용하여, 동적인 기능을 추가했어요"
 }
 
 function hide() {
