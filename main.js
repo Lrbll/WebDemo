52bd4b5497e003a4f87b4321339ea2128fdb1847
Lrbll <dlxkdxs@naver.com>
2023-03-16 02:09:38 +0900

    2nd

--- main.js
+++ main.js
@@ -4,8 +4,28 @@ function pink() {
   document.body.style.backgroundColor = "pink"
 }
 
+function lemon() {
+  document.body.style.backgroundColor = "lemonchiffon"
+}
+
+function purple() {
+  document.body.style.backgroundColor = "lavender"
+}
+
+function reset() {
+  document.body.style.backgroundColor = "rgb(195, 230, 210)"
+}
+
 function showHTML() {
   document.getElementById("fig").src = "img/html.png"
+}
+
+function showCSS() {
+  document.getElementById("fig").src = "img/css.png"
+}
+
+function showJS() {
+  document.getElementById("fig").src = "img/JS.png"
 }
 
 function hide() {
